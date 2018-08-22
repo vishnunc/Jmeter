cacddebf8b9899fb5326c2b8747cf5ed3218c80c
vishnunc <31437273+vishnunc@users.noreply.github.com>
2018-08-23 00:24:43 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-C:\Jmeter\apache-jmeter-3.3\apache-jmeter-3.3\bin\jmeter -n -t 
+C:\Jmeter\apache-jmeter-3.3\apache-jmeter-3.3\bin\jmeter -Jjmeter.save.saveservice.output_format=csv -n -t Jmeter-Jenkins-ACME-Demo_V1.jmx -l Result.jtl
